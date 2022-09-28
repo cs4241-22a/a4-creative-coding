e0734bf9d926a9759f9e634d5a56f269d153e4da
2bias <tmack@wpi.edu>
2022-09-28 19:10:14 -0400

    floor with movable sphere

--- public/main.js
+++ public/main.js
@@ -1,69 +1,175 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
+//import { GLTFLoader } from 'https://unpkg.com/three@0.127.0/examples/jsm/loaders/GLTFLoader.js';
 
+let sphere;
+
+// CAMERA
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1500);
+camera.position.set(150, 150, 0);
+camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+// RENDERER
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.shadowMap.enabled = true;
+document.body.appendChild(renderer.domElement);
+
+// WINDOW RESIZE HANDLING
+export function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', onWindowResize);
+
+// SCENE
 const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xbfd1e5);
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.z = 2
+// CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement);
+
+export function animate() {
+  dragObject();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+// ambient light
+let hemiLight = new THREE.AmbientLight(0xffffff, 0.20);
+scene.add(hemiLight);
+
+//Add directional light
+let dirLight = new THREE.DirectionalLight(0xffffff, 1);
+dirLight.position.set(-30, 50, -30);
+scene.add(dirLight);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize.width = 2048;
+dirLight.shadow.mapSize.height = 2048;
+dirLight.shadow.camera.left = -70;
+dirLight.shadow.camera.right = 70;
+dirLight.shadow.camera.top = 70;
+dirLight.shadow.camera.bottom = -70;
+
+function createFloor() {
+  let pos = { x: 0, y: -1, z: 3 };
+  let scale = { x: 100, y: 2, z: 100 };
+
+  let blockPlane = new THREE.Mesh(new THREE.BoxBufferGeometry(),
+       new THREE.MeshPhongMaterial({ color: 0xf9c834 }));
+  blockPlane.position.set(pos.x, pos.y, pos.z);
+  blockPlane.scale.set(scale.x, scale.y, scale.z);
+  blockPlane.castShadow = true;
+  blockPlane.receiveShadow = true;
+  scene.add(blockPlane);
+
+  blockPlane.userData.ground = true
+}
+
+
+function createSphere() {
+  let radius = 4;
+  let pos = { x: 15, y: radius, z: -15 };
+
+  const axesHelper = new THREE.AxesHelper( 51 );
+  scene.add( axesHelper );
+
+  sphere = new THREE.Mesh(new THREE.SphereBufferGeometry(radius, 32, 32), 
+      new THREE.MeshPhongMaterial({ color: 0x43a1f4 }))
+  sphere.position.set(10, 4, 10)
+  //camera.position.set(100, 500, 100)
+ // camera.position.set(10,1,1)
+  sphere.castShadow = true
+  sphere.receiveShadow = true
+  scene.add(sphere)
+
+
+  sphere.userData.draggable = true
+  sphere.userData.name = 'SPHERE'
+}
 
 
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const raycaster = new THREE.Raycaster(); // create once
+const clickMouse = new THREE.Vector2();  // create once
+const moveMouse = new THREE.Vector2();   // create once
+var draggable;
+
+function intersect(pos) {
+  raycaster.setFromCamera(pos, camera);
+  return raycaster.intersectObjects(scene.children);
+}
 
 
-const geometry = new THREE.BoxGeometry()
-const material = new THREE.MeshBasicMaterial({
-    color: 0xabcdefa,
-    wireframe: true
+
+
+//EVENT LISTENERS
+
+window.addEventListener('click', event => {
+  if (draggable != null) {
+    console.log(`dropping draggable ${draggable.userData.name}`)
+    draggable = null
+    return;
+  }
+
+  // THREE RAYCASTER
+  clickMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  clickMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+
+  const found = intersect(clickMouse);
+  if (found.length > 0) {
+    if (found[0].object.userData.draggable) {
+      draggable = found[0].object
+      console.log(`found draggable ${draggable.userData.name}`)
+    }
+  }
 })
-const cube = new THREE.Mesh(geometry, material)
-scene.add(cube)
+
+window.addEventListener('mousemove', event => {
+  moveMouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  moveMouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+});
+
+window.addEventListener('keydown', (event) => {
+    console.log(event.key)
+
+    let pressed = (event.key).toLowerCase()
+    if (pressed === 'w') {
+        sphere.position.x -= 1.5;
+    } else if (pressed === 'a') {
+        sphere.position.z += 1.5;
+    } else if (pressed === 's') {
+        sphere.position.x += 1.5;
+    } else if (pressed === 'd') {
+        sphere.position.z -= 1.5;
+    }
+
+})
 
 
-window.addEventListener('resize', onWindowResize, false)
 
 
-
-
-
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight
-    camera.updateProjectionMatrix()
-    renderer.setSize(window.innerWidth, window.innerHeight)
-    render()
+//FUNCTIONS
+function dragObject() {
+  if (draggable != null) {
+    const found = intersect(moveMouse);
+    if (found.length > 0) {
+      for (let i = 0; i < found.length; i++) {
+        if (!found[i].object.userData.ground)
+          continue
+        
+        let target = found[i].point;
+        draggable.position.x = target.x
+        draggable.position.z = target.z
+      }
+    }
+  }
 }
 
-function animate() {
-    requestAnimationFrame(animate)
 
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+createFloor()
+createSphere()
 
-    render()
-}
-
-function scroll(){
-    camera.position.z -= -1
-}
-
-
-function render() {
-    renderer.render(scene, camera)
-}
-
-
-
-
-
-
-//window.onscroll = scroll;
 animate()
-
-
-
