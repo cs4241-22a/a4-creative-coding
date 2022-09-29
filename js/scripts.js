64620d96b93226c5dd687c8db9344ce94596b201
Tia Mehta <tmehta@wpi.edu>
2022-09-29 12:21:03 -0700

    added some stuff

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
+import Tweakpane from 'tweakpane';
 console.log("script is connected");
 
 const pane = new Tweakpane({
-    container: document.getElementById('container')
+    container: document.querySelector('#container'),
 });
+
+const PARAMS = {
+    factor: 123,
+    title: 'hello',
+    color: '#ff0055',
+};
+
+pane.addInput(PARAMS, 'factor');
+pane.addInput(PARAMS, 'title');
+pane.addInput(PARAMS, 'color');
