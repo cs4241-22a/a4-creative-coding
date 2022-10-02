ae9fc14f5eacf4420a3ae366101119a188113735
Glitch (concise-experienced-twist) <none>
2022-10-02 07:15:02 +0000

    Upaded tweakpane options and added upload audio file option

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,30 @@
-
 import * as THREE from "https://unpkg.com/three/build/three.module.js";
-
-
+import "https://cdn.jsdelivr.net/npm/tweakpane@3.0.5/dist/tweakpane.min.js";
 
 const app = {
   init() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("audio-upload").style.display = "none";
+    document.getElementById("audio-label").style.display = "none";
+
     app.scene = new THREE.Scene();
     const listener = new THREE.AudioListener();
     const sound = new THREE.Audio(listener);
     const audioLoader = new THREE.AudioLoader();
 
+    const fieldOfView = 75;
+    const aspect = 2;
+    const near = 2;
+    const far = 1;
 
-    app.camera = new THREE.PerspectiveCamera();
-    app.camera.position.z = 50;
+    app.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    app.camera.position.z = 3;
     app.camera.add(listener);
 
     app.renderer = new THREE.WebGLRenderer();
@@ -20,95 +32,130 @@ const app = {
 
     document.body.appendChild(app.renderer.domElement);
 
-   const pointLight = new THREE.PointLight(0xffffff);
-   pointLight.position.z = 100;
-   app.scene.add(pointLight); 
+    const light = new THREE.PointLight( 0xff0000, 1, 100 );
+    light.position.set( 50, 50, 50 );
+    app.scene.add( light );
 
-   app.knot = app.createKnot();
-
+    app.cube = app.makeCube();
 
     app.render = app.render.bind(app);
+    if (app.audioCtx == null) {
+      const audioCtx = new AudioContext();
+      const audioElement = document.createElement("audio");
+      audioElement.controls = true;
+      audioElement.reload = "auto";
+      document.body.appendChild(audioElement);
 
-    
-	  audioLoader.load( 'https://cdn.glitch.global/163f56bd-2a47-4f3d-b6f0-a42072d923b0/mixkit-mystwrious-bass-pulse-2298.wav?v=1664503743586', function( buffer ) {
-	  sound.setBuffer( buffer );
-	  sound.setLoop( true );
-	  sound.setVolume( 0.5 );
-	  sound.play();
-	  })
-   
+      app.analyser = audioCtx.createAnalyser();
+      app.analyser.fftSize = 1024;
+      const player = audioCtx.createMediaElementSource(audioElement);
+      player.connect(audioCtx.destination);
+      player.connect(app.analyser);
 
-    const audioCtx = new AudioContext();
-    const audioElement = document.createElement("audio");
-    document.body.appendChild(audioElement);
-
-    app.analyser = audioCtx.createAnalyser();
-    app.analyser.fftSize = 1024; // 512 bins
-    const player = audioCtx.createMediaElementSource(audioElement);
-    player.connect(audioCtx.destination);
-    player.connect(app.analyser);
-
-
-    audioElement.src = 'mixkit-mystwrious-bass-pulse-2298.wav'
-    audioElement.play();
+      audioElement.src = "GoodVibes.mp3";
+      audioElement.play();
+    }
 
     app.results = new Uint8Array(app.analyser.frequencyBinCount);
-
-
-    const pane = new Tweakpane.Pane();
-
-    const objcolor = 0x0088ff;
-
-    const PARAMS = {
-      song: "Song title",
-      dodecahedron_color: 0x0088ff,
-      volume: 0.5,
-
-    };
-
-    pane.addInput(PARAMS, 'song')
-
-    pane.addInput(PARAMS, 'dodecahedron_color', {
-      view: 'color',
-    });
-
-    pane.addInput(PARAMS, 'volume', {
-      min: 0,
-      max: 1,
-    });
-
 
     app.render();
   },
 
-  createKnot() {
-    const knotgeo = new THREE.DodecahedronGeometry(10,0);
-    const mat = new THREE.MeshPhongMaterial({
-      color: 0x0088ff,
-      shininess: 2000
+  makeCube() {
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({
+      color: "#b2b2b2",
+      wireframe: true,
     });
-    const knot = new THREE.Mesh(knotgeo, mat);
-    knot.wireframe = true;
 
-    app.scene.add(knot);
-    return knot;
+    const cube = new THREE.Mesh(geometry, material);
+    app.scene.add(cube);
+    const pane = new Tweakpane.Pane();
+
+    const color = pane.addInput({ color: "#b2b2b2" }, "color", {
+      view: "color", // show values as hex
+    });
+    color.on("change", (v) => {
+      material.color = new THREE.Color(v.value);
+    });
+
+    const x = pane.addInput({ x: 1 }, "x", {
+      min: 0,
+      step: 1,
+      max: 5,
+    });
+    x.on("change", (x) => {
+      cube.scale.x = x.value;
+    });
+
+    const y = pane.addInput({ y: 1 }, "y", {
+      min: 0,
+      step: 1,
+      max: 5,
+    });
+    y.on("change", (y) => {
+      cube.scale.y = y.value;
+    });
+
+    const z = pane.addInput({ z: 1 }, "z", {
+      min: 0,
+      step: 1,
+      max: 5,
+    });
+    z.on("change", (z) => {
+      cube.scale.z = z.value;
+    });
+    const wireframe = pane.addInput({ wireframe: true }, "wireframe");
+    wireframe.on("change", (w) => {
+      material.wireframe = w.value;
+    });
+    return cube;
   },
 
   render() {
     app.analyser.getByteFrequencyData(app.results);
-
     for (let i = 0; i < app.analyser.frequencyBinCount; i++) {
-      app.knot.rotation.x += app.results[i];
-      app.knot.rotation.y += app.results[i];
-      app.knot.shininess += app.results[i];
-      app.knot.rotation.z += app.results[i];
-
+      app.cube.rotation.x += app.results[i] * 0.000002;
+      app.cube.rotation.y += app.results[i] * 0.000002;
+      app.cube.rotation.z += app.results[i] * 0.000002;
     }
     app.renderer.render(app.scene, app.camera);
     window.requestAnimationFrame(app.render);
-  }
+  },
 };
 
+const start = document.getElementById("start");
+start.addEventListener("click", app.init);
 
-const playButton = document.getElementById( 'playButton' );
-playButton.addEventListener( 'click', app.init );
+function changeHandler({ target }) {
+  // Make sure we have files to use
+  if (!target.files.length) return;
+
+  // Create a blob that we can use as an src for our audio element
+  const urlObj = URL.createObjectURL(target.files[0]);
+
+  // Create an audio element
+  app.audioCtx = new AudioContext();
+  const audioElement = document.createElement("audio");
+  audioElement.controls = true;
+  audioElement.reload = "auto";
+  document.body.appendChild(audioElement);
+
+  app.analyser = app.audioCtx.createAnalyser();
+  app.analyser.fftSize = 1024;
+  const player = app.audioCtx.createMediaElementSource(audioElement);
+  player.connect(app.audioCtx.destination);
+  player.connect(app.analyser);
+
+  audioElement.addEventListener("load", () => {
+    URL.revokeObjectURL(urlObj);
+  });
+
+  audioElement.src = urlObj;
+  audioElement.play();
+  app.init();
+}
+
+document
+  .getElementById("audio-upload")
+  .addEventListener("change", changeHandler);
--- server.js
+++ server.js
@@ -2,34 +2,16 @@ const express = require('express'),
       app     = express(),
       cors    = require('cors'),
       path = require('path'),
-      router = express.Router(),
-      port = process.env.PORT || 3000
-/*
-app.get('/', function(req, res){
-    res.render('index.ejs');});
-*/
+      router = express.Router()
 
 app.use( cors() )
 
-// make all the files available
-// https://expressjs.com/en/starter/static-files.html
 app.use( express.static('public') )
 app.use( express.static('assets') )
 
-// https://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
- // console.log(__dirname);
-
 });
 
 
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-    console.log("Your app is listening on port " + listener.address().port);
-  //  console.log(__dirname);
-
-});
-
-//app.listen( 3000 )
+app.listen(process.env.PORT || 3000)
