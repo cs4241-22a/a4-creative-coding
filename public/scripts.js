ffeb16e1921abe769a7ab83babdecbc4ea4cf602
hyoder <hryoder@wpi.edu>
2022-10-02 19:06:35 -0400

    init

---
+++ public/scripts.js
@@ -0,0 +1,35 @@
+let     audio = new Audio(),
+       source = null,
+     analyzer = null;
+    audio.src = "./public/source.m4a";
+const context = new (window.AudioContext || window.webkitAudioContext)(),
+    container = document.getElementById( "container" ),
+       canvas = document.getElementById( "canvas" );
+canvas.width  = window.innerWidth;
+canvas.height = window.innerHeight;
+
+audio.play();
+source = context.createMediaElementSource( audio );
+analyzer = context.createAnalyser();
+source.connect( analyzer );
+analyzer.connect( context.destination );
+
+analyzer.fftSize = 128;
+const buflen = analyzer.frequencyBinCount,
+        data = new Uint8Array( buflen ),
+    barwidth = canvas.width / buflen;
+
+function animate()
+{
+    x = 0;
+    context.clearRect( 0, 0, canvas.width, canvas.height );
+    analyzer.getByteFrequencyData( data );
+    for ( let i = 0 ; i < buflen ; i++ )
+    {
+        barheight = data[i];
+        context.fillStyle = "white";
+        context.fillRect( x, canvas.height - barheight, barwidth, barheight );
+        x += barwidth;
+    }
+    requestAnimationFrame(animate);
+}
