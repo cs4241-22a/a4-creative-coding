8f2cc2196189a32c3e82eef29bae758018f378e8
Cole Manning // RVRX <colemanning@yandex.com>
2022-10-02 11:10:17 -0400

    Add track credits

--- routes/visualizer.js
+++ routes/visualizer.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET visualizer page. */
 router.get('/', function(req, res, next) {
-    res.render('visualizer', { title: 'Visualizer' });
+    res.render('visualizer', { title: 'Visualizer', track: 'Dylan Ross - Angel Juan' });
 });
 
 module.exports = router;
