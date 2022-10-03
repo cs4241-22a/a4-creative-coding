6d8f573c3f1e78df4fafe54370fe9ad24f7d3d89
hyoder <hryoder@wpi.edu>
2022-10-02 22:16:33 -0400

    fix three i hope

--- public/scripts.js
+++ public/scripts.js
@@ -33,14 +33,14 @@ control.addEventListener('click', () =>
     }
 }, false );
 
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 25, window.innerWidth/window.innerHeight, 0.1, 1000 );
-var renderer = new THREE.WebGLRenderer();
+var scene = new Scene();
+var camera = new PerspectiveCamera( 25, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
-var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-var material = new THREE.MeshBasicMaterial( { color: 0xaaaaaa } );
-var cube = new THREE.Mesh( geometry, material );
+var geometry = new BoxGeometry( 1, 1, 1 );
+var material = new MeshBasicMaterial( { color: 0xaaaaaa } );
+var cube = new Mesh( geometry, material );
 scene.add( cube );
 camera.position.z = 5;
 
