b6bc5d95026a73879230f12c2fb6cf9284afeca1
Glitch (a4-rluu1) <none>
2022-10-03 21:24:56 +0000

    glitch compatible

--- public/script.js
+++ public/script.js
@@ -1,29 +1,26 @@
 const margin = { top: 50, right: 50, bottom: 50, left: 50 },
-    height = 300,
-    width = 300
+  height = 300,
+  width = 300;
 
-let pokemonList= []
+let pokemonList = [];
 
-window.onload = function() {
-    fetch( "./pokedex.json" )
-      .then(function(resp) {
-          return resp.json();
-      })
-      .then(function(data) {
-          console.log(data);
-          data['pokemon'].forEach(function (dset) {
-            var pikaPokemon = {
-              'name': dset['name'],
-              'type': dset['type'],
-              'id': dset['id'],
-              'img': dset['img'],
-              'candy': dset['candy'],
-              'candy_count': dset['candy_count']
-            }
-            pokemonList.push(pikaPokemon);
-          }
-      )}
-      )
-  }
-
-
+window.onload = function () {
+  fetch("./pokedex.json")
+    .then(function (resp) {
+      return resp.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      data["pokemon"].forEach(function (dset) {
+        var pikaPokemon = {
+          name: dset["name"],
+          type: dset["type"],
+          id: dset["id"],
+          img: dset["img"],
+          candy: dset["candy"],
+          candy_count: dset["candy_count"],
+        };
+        pokemonList.push(pikaPokemon);
+      });
+    });
+};
