30cc98de00da83586b25c6840d51f5016b78226f
hyoder <hryoder@wpi.edu>
2022-10-02 20:38:53 -0400

    commit 6 we pray

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,9 @@
 let     audio = new Audio();
     audio.src = "/source.mp3";
 const canvas  = document.getElementById( "canvas" ),
-     audioctx = new window.AudioContext();
+      control = document.getElementById( "playbutton" );
+      context = new window.AudioContext();
+audio.addEventListener('ended', () => { control.dataset.state = 'off'; console.log('ended'); }, false );
 canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
 let source = context.createMediaElementSource( audio ),
@@ -13,37 +15,43 @@ let buflen = analyzer.frequencyBinCount,
 data = new Uint8Array( buflen )
 barwidth = canvas.width / buflen;
 
-const button = document.getElementById( "startbutton" );
-button.addEventListener('click' () => 
+
+
+control.addEventListener('click', () => 
 {
-    if(  === 'suspended' )
+    if( context.state === 'suspended' )
     {
         context.resume();
-        console.log('audio resumed');
+        console.log('resume');
     }
-} );
-function starter()
-{
-    audio.play();
-    animate();
-}
+    if( control.dataset.state === 'off' )
+    {
+        audio.play();
+        control.dataset.state = 'on';
+        console.log('play');
+    }
+    if( control.dataset.state === 'on' )
+    {
+        audio.pause();
+        control.dataset.state = 'off';
+        console.log('pause');
+    }
+}, false );
 
 function animate()
 {
-    x = 0;
-    ctx.clearRect( 0, 0, canvas.width, canvas.height );
-    analyzer.getByteFrequencyData( data );
-    for ( let i = 0 ; i < buflen ; i++ )
+    if( control.dataset.state === 'on' )
     {
-        barheight = data[i];
-        ctx.fillStyle = "white";
-        ctx.fillRect( x, canvas.height - barheight, barwidth, barheight );
-        x += barwidth;
+        x = 0;
+        ctx.clearRect( 0, 0, canvas.width, canvas.height );
+        analyzer.getByteFrequencyData( data );
+        for ( let i = 0 ; i < buflen ; i++ )
+        {
+            barheight = data[i];
+            ctx.fillStyle = "white";
+            ctx.fillRect( x, canvas.height - barheight, barwidth, barheight );
+            x += barwidth;
+        }
     }
     requestAnimationFrame(animate);
 }
-window.onload = function()
-{
-    const startbutton = document.getElementById( "startbutton" );
-    startbutton.onclick = starter;
-};
