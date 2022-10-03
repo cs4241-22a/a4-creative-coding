8bac4d2eda0456653c61cc17e487eff942455f79
Cather Zhang <cather0215@gmail.com>
2022-10-02 21:15:02 -0400

    done?

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+var cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/view/index.html");
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log("Your app is listening on port " + listener.address().port);
+  });
