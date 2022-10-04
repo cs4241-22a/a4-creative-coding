4497f25f64632d5c7351afc65783a4382e6cd7bb
rmbrunelle <rmbrunelle2@wpi.edu>
2022-10-04 01:07:06 -0400

    finishing up

--- public/js/client.js
+++ public/js/client.js
@@ -8,6 +8,7 @@ console.log("in client script")
     var canvasTest = d3.select("body").append("svg")
         .attr("width", 750)
         .attr("height", 3050)
+        .attr("id", "svgBars")
         //.call(zoom)
         console.log("before rect")
 
@@ -31,8 +32,6 @@ console.log("in client script")
         console.log("after text")
 
 
-    // let growthTitle = d3.select("body").append("h2")
-    // d3.select('h2').text('Continuous Population Growth')
     let growthContainer = d3.select("#continuous").append("svg")
     .attr("width", 500)
     .attr("height", 500)
@@ -64,57 +63,9 @@ console.log("in client script")
     )
     .attr("text", "US Population 1960-2021")
 
-
-
-
-        const PARAMS = {
-            factor: 123,
-            title: 'hello',
-            color: 'ff0055',
-            count: 10
-        }
-        
-        const pane = new Tweakpane.Pane()
-        
-        const fIn = pane.addInput(PARAMS, 'factor')
-        const tIn = pane.addInput(PARAMS, 'title')
-        const cIn = pane.addInput(PARAMS, 'color')
-        const cntIn = pane.addInput(PARAMS, 'count')
-        
-        tIn.on('change', function(ev) {
-            console.log(`change: ${ev.value}`);
-            d3.select('h1').text(ev.value)
-        })
-        
-        cIn.on('change', function(ev) {
-            d3.select('svg').selectAll("rect").attr("fill", '#'+ev.value)
-        })
-        
-
-
-
     })
 
 console.log("end of script")
-
-
-
-// function zoom(svg) {
-//     const extent = [[0, 0], [750 - 0, 3050 - 0]];
-  
-//     svg.call(d3.zoom()
-//         .scaleExtent([1, 8])
-//         .translateExtent(extent)
-//         .extent(extent)
-//         .on("zoom", zoomed));
-  
-//     function zoomed(event) {
-//       d3.scaleLinear().range([0, 750 - 0].map(d => event.transform.applyX(d)));
-//       svg.selectAll("rect").attr("x", d => applyX(d.name)).attr("width", 10);
-//       svg.selectAll(".x-axis").call(xAxis);
-//     }
-//   }
-
 
 
 let growthData = {x: [1960, 1970, 1980, 1990, 2000, 2010, 2020], y: [2200600, 2000300, 1959400, 2941700, 2460911.8, 1900281, 1369892]}
@@ -144,46 +95,21 @@ var path = growthSvg.append("path")
             .attr("fill", "none")
             .attr("stroke", "black")  
 
+growthSvg.append("g")
+    .attr("transform", "translate(0,"+500+ ")")
+    .attr("class", "x axis")
+    .call(d3.axisBottom(x))
 
-// let growthContainer = d3.select("body").append("svg")
-//     .attr("width", 500)
-//     .attr("height", 500)
-
-//     var x = d3.scaleTime()
-//     .domain(d3.extent(data, function(d) { return d.date; }))
-//     .range([ 0, 500 ]);
-//   growthContainer.append("g")
-//     .attr("transform", "translate(0," + 500 + ")")
-//     .call(d3.axisBottom(x));
-
-//   // Add Y axis
-//   var y = d3.scaleLinear()
-//     .domain([0, d3.max(data, function(d) { return +d.value; })])
-//     .range([ 500, 0 ]);
-//   growthContainer.append("g")
-//     .call(d3.axisLeft(y))
-
-// growthContainer.append("path")
-//     .datum(growthData)
-//     .attr("fill", "none")
-//     .attr("stroke", "steelblue")
-//     .attr("stroke-width", 1.5)
-//     .attr("d", d3.line()
-//     .x(function(d) { return x(d.date) })
-//     .y(function(d) { return y(d.value) })
-//     )
-
-
-// let circles = growthContainer.selectAll("circle")
-//     .data(growthData)
-//     .enter()
-//     .append("circle")
+growthSvg.selectAll(".dot")
+    .data(growthData)
+    .enter().append("circle")
+    .attr("class", "dot")
+    .attr("cy", function(d) { return yScale(d.y) })
+    .attr("r", 5);
 
 
 
 am5.ready(function() {
-
-
 
     var allData = {
         "0": {
@@ -285,26 +211,17 @@ var root = am5.Root.new("chartdiv");
 root.numberFormatter.setAll({
   numberFormat: "#a",
 
-  // Group only into M (millions), and B (billions)
   bigNumberPrefixes: [
     { number: 1e6, suffix: "M" },
     { number: 1e9, suffix: "B" }
   ],
-
-  // Do not use small number prefixes at all
   smallNumberPrefixes: []
 });
 
 var stepDuration = 2000;
 
-
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
 root.setThemes([am5themes_Animated.new(root)]);
 
-
-// Create chart
-// https://www.amcharts.com/docs/v5/charts/xy-chart/
 var chart = root.container.children.push(am5xy.XYChart.new(root, {
   panX: true,
   panY: true,
@@ -312,18 +229,12 @@ var chart = root.container.children.push(am5xy.XYChart.new(root, {
   wheelY: "none"
 }));
 
-
-// We don't want zoom-out button to appear while animating, so we hide it at all
 chart.zoomOutButton.set("forceHidden", true);
 
-
-// Create axes
-// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
 var yRenderer = am5xy.AxisRendererY.new(root, {
   minGridDistance: 20,
   inversed: true
 });
-// hide grid
 yRenderer.grid.template.set("visible", false);
 
 var yAxis = chart.yAxes.push(am5xy.CategoryAxis.new(root, {
@@ -343,9 +254,6 @@ var xAxis = chart.xAxes.push(am5xy.ValueAxis.new(root, {
 xAxis.set("interpolationDuration", stepDuration / 10);
 xAxis.set("interpolationEasing", am5.ease.linear);
 
-
-// Add series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
 var series = chart.series.push(am5xy.ColumnSeries.new(root, {
   xAxis: xAxis,
   yAxis: yAxis,
@@ -353,19 +261,14 @@ var series = chart.series.push(am5xy.ColumnSeries.new(root, {
   categoryYField: "network"
 }));
 
-// Rounded corners for columns
 series.columns.template.setAll({ cornerRadiusBR: 5, cornerRadiusTR: 5 });
-
-// Make each column to be of a different color
 series.columns.template.adapters.add("fill", function (fill, target) {
   return chart.get("colors").getIndex(series.columns.indexOf(target));
 });
-
 series.columns.template.adapters.add("stroke", function (stroke, target) {
   return chart.get("colors").getIndex(series.columns.indexOf(target));
 });
 
-// Add label bullet
 series.bullets.push(function () {
   return am5.Bullet.new(root, {
     locationX: 1,
@@ -389,7 +292,6 @@ var label = chart.plotContainer.children.push(am5.Label.new(root, {
   centerX: am5.p100
 }));
 
-// Get series item by category
 function getSeriesItem(category) {
   for (var i = 0; i < series.dataItems.length; i++) {
     var dataItem = series.dataItems[i];
@@ -399,31 +301,20 @@ function getSeriesItem(category) {
   }
 }
 
-// Axis sorting
 function sortCategoryAxis() {
-  // sort by value
   series.dataItems.sort(function (x, y) {
     return y.get("valueX") - x.get("valueX"); // descending
-    //return x.get("valueX") - y.get("valueX"); // ascending
   });
 
-  // go through each axis item
   am5.array.each(yAxis.dataItems, function (dataItem) {
-    // get corresponding series item
     var seriesDataItem = getSeriesItem(dataItem.get("category"));
-
     if (seriesDataItem) {
-      // get index of series data item
       var index = series.dataItems.indexOf(seriesDataItem);
-      // calculate delta position
       var deltaPosition =
         (index - dataItem.get("index", 0)) / series.dataItems.length;
-      // set index to be the same as series data item index
       if (dataItem.get("index") != index) {
         dataItem.set("index", index);
-        // set deltaPosition instanlty
         dataItem.set("deltaPosition", -deltaPosition);
-        // animate delta position to 0
         dataItem.animate({
           key: "deltaPosition",
           to: 0,
@@ -433,20 +324,52 @@ function sortCategoryAxis() {
       }
     }
   });
-  // sort axis items by index.
-  // This changes the order instantly, but as deltaPosition is set, they keep in the same places and then animate to true positions.
   yAxis.dataItems.sort(function (x, y) {
     return x.get("index") - y.get("index");
   });
 }
 
-var year = 0;
+let startYear = 0
+let endYear = 9
 
-// update data with values each 1.5 sec
+
+const PARAMS = {
+    step: 2000,
+    start: 0,
+    color: 'ff0055',
+    end: 9
+}
+
+const pane = new Tweakpane.Pane()
+
+const stIn = pane.addInput(PARAMS, 'step')
+const sIn = pane.addInput(PARAMS, 'start')
+const cIn = pane.addInput(PARAMS, 'color')
+const eIn = pane.addInput(PARAMS, 'end')
+
+stIn.on('change', function(ev) {
+    stepDuration = ev.value
+})
+
+cIn.on('change', function(ev) {
+    d3.select('svgBars').selectAll("rect").attr("fill", '#'+ev.value)
+})
+
+sIn.on('change', function(ev) {
+    startYear = ev.value
+})
+
+eIn.on('change', function(ev) {
+    endYear = ev.value
+})
+
+
+var year = startYear;
+
 var interval = setInterval(function () {
   year++;
 
-  if (year > 9) {
+  if (year > endYear) {
     clearInterval(interval);
     clearInterval(sortInterval);
   }
@@ -498,21 +421,12 @@ function updateData() {
     yAxis.zoom(0, itemsWithNonZero / yAxis.dataItems.length);
   }
 }
-
 setInitialData();
 setTimeout(function () {
   year++;
   updateData();
 }, 50);
 
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
 series.appear(1000);
 chart.appear(1000, 100);
-
-}); // end am5.ready()
-
-
-
-
-
+}); 
--- server.js
+++ server.js
@@ -3,15 +3,6 @@
 const express = require('express'), app = express()
 
 app.use( express.urlencoded({ extended:true }) )
-
-
-// app.use( (req, res, next) => {
-//     if (collection !== null) {
-//       next();
-//     } else {
-//       res.status(503).send();
-//     }
-//   });
   
 app.use(express.static('./public'))
 console.log('in server')
@@ -24,20 +15,3 @@ app.get( '/', (req,res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log('listening on port 3000'))
-
-
-
-
-// // const Tweakpane = require('tweakpane');
-// const myPane = new Tweakpane();
-
-// // Parameter object
-// const PARAMS = {
-//     rotationRate: 0.5,
-//     anotherParam: 'test'
-//     };
-//     // Pass the object and its key to pane
-//     myPane.addInput(PARAMS, 'rotationRate');
-//     // Second Param
-//     myPane.addInput(PARAMS, 'anotherParam');
-
