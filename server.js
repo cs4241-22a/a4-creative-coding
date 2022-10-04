efab3bc7537701ec8730f4f6e88af2e7a98f186f
Colb131 <cfrechette@wpi.edu>
2022-10-04 18:59:01 -0400

    Finished

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
-const express = require( 'express' ),
-    app = express()
+// server.js
+// where your node app starts
 
-app.use( express.static( 'public' ) )
+// init project
+const express = require("express");
+const app = express();
 
-app.listen( process.env.PORT || 3000 )
+// we've started you off with Express,
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function(request, response) {
+    response.sendFile(__dirname + "/views/index.html");
+});
+
+// listen for requests :)
+const listener = app.listen(process.env.PORT, function() {
+    console.log("Your app is listening on port " + listener.address().port);
+});
