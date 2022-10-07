566ef78bf2663700491cc8adfa4e80390f696de1
Tia Mehta <tmehta@wpi.edu>
2022-10-07 02:15:55 -0400

    ajkfjf

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,11 @@ console.log("script is connected");
 const start = function() {
   const audioCtx = new AudioContext()
   const audioElem = document.createElement('audio')
-  source = audioCtx.createBufferSource()
 
   audioElem.src = '/sound1.ogg'
   audioElem.play()
-
-  audioCtx.decodeAudioData(
-    audioElem,
-    (buffer) => {
-      myBuffer = buffer
-      source.buffer = myBuffer
-      source.playbackRate.value = playbackControl.value
-      source.connect(audioCtx.destination)
-      source.loop = true
-    }
-  )
 }
+
 
 window.onload = function() {
   const pane = new Tweakpane.Pane();
